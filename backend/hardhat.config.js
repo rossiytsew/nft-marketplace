2e5716393fc0ad8023f735de7b899c2b74f615eb
rossiytsew <slavarossiytsev@gmail.com>
2023-06-17 20:30:49 +0300

    Adding deploy script for nft marketplace smart contract.

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -48,6 +48,7 @@ module.exports = {
             //   },
             saveDeployments: true,
             chainId: 11155111,
+            blockConfirmations: 6,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
@@ -57,12 +58,14 @@ module.exports = {
             //   },
             saveDeployments: true,
             chainId: 1,
+            blockConfirmations: 6,
         },
         polygon: {
             url: POLYGON_MAINNET_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             saveDeployments: true,
             chainId: 137,
+            blockConfirmations: 6,
         },
     },
     etherscan: {
