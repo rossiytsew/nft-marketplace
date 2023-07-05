6927404c2eb755b5b9a84b58d721489de4e411ca
rossiytsew <slavarossiytsev@gmail.com>
2023-07-05 10:16:54 +0300

    Refactoring of basic nft contract.

--- backend/contracts/test/BasicNft.sol
+++ backend/contracts/test/BasicNft.sol
@@ -8,19 +8,20 @@ contract BasicNft is ERC721 {
     string public constant TOKEN_URI = "ipfs://QmPWHk2bPH4hqJLqBVtsTiEZaHwR9t7hViJk5vfPMS2vYx?filename=corgi-jpeg.json";
     uint256 private s_tokenCounter;
 
+    event DogMinted(uint256 indexed tokenId);
+
     constructor() ERC721("CORGI", "CRG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public returns (uint256) {
+    function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        emit DogMinted(s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
-        return s_tokenCounter;
     }
 
-    function tokenURI(
-        uint256 /* tokenId */
-    ) public pure override returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return TOKEN_URI;
     }
 
