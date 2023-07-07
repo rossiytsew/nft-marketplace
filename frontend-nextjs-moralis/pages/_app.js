29ef9b8daba7c2969058151c80c6e1b850655166
rossiytsew <slavarossiytsev@gmail.com>
2023-07-07 10:32:46 +0300

    Frontend NextJS Moralis app initial creation.

---
+++ frontend-nextjs-moralis/pages/_app.js
@@ -0,0 +1,12 @@
+import '@/styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import Header from '@/components/Header'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <Header />
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
+}
